fcd8f40380bffbc1c26a8c51cc5fb3b12b4711dc
xislam <nurdinovislam4@gmail.com>
2021-05-16 18:16:34 +0600

    создал базы данных и исправел верстку путей css & js

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -36,4 +36,99 @@ class AboutUs(models.Model):
     title = models.CharField(max_length=400, verbose_name='title')
     text = models.CharField()
 
-class
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'About Us'
+        verbose_name_plural = 'About Us'
+
+
+class DailyEvents(models.Model):
+    img = models.ImageField(upload_to='events', verbose_name='img')
+    title = models.CharField(max_length=400, verbose_name='title')
+    subtitle = models.CharField(max_length=1000, verbose_name='subtitle')
+    text = models.CharField(max_length=1000, verbose_name='text')
+    data = models.CharField(max_length=1000, verbose_name='text_data')
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name='created date')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Daily Events'
+        verbose_name_plural = 'Daily Events'
+
+
+class SportEvents(models.Model):
+    img = models.ImageField(upload_to='events', verbose_name='img')
+    title = models.CharField(max_length=400, verbose_name='title')
+    subtitle = models.CharField(max_length=1000, verbose_name='subtitle')
+    text = models.CharField(max_length=1000, verbose_name='text')
+    data = models.CharField(max_length=1000, verbose_name='text_data')
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name='created date')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Sport Events'
+        verbose_name_plural = 'Sport Events'
+
+
+class Slider(models.Model):
+    img = models.ImageField(upload_to="slider", verbose_name="img")
+    name = models.CharField(max_length=3000, verbose_name="name img")
+    created_date = models.DateTimeField(auto_now_add=True, verbose_name='created date')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Slider'
+        verbose_name_plural = 'Slider'
+
+
+class RESERVATIONS(models.Model):
+    text = models.CharField(max_length=3000, verbose_name='text')
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        verbose_name = 'RESERVATIONS'
+        verbose_name_plural = 'RESERVATIONS'
+
+
+class OpenTime(models.Model):
+    time = models.CharField(max_length='300', verbose_name='open time')
+
+    def __str__(self):
+        return self.time
+
+    class Meta:
+        verbose_name = 'Open Time'
+        verbose_name_plural = 'Open Time'
+
+
+class OurLocation(models.Model):
+    text = models.CharField(max_length='300', verbose_name='Our Location')
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        verbose_name = 'Our Location'
+        verbose_name_plural = 'Our Location'
+
+
+class TableReservation(models.Model):
+    phone = models.CharField(max_length=300, verbose_name='phone')
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.phone
+
+    class Meta:
+        verbose_name = 'Table Reservation'
+        verbose_name_plural = 'Table Reservation'
